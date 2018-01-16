0bc3061adc5a5eeac0d2b7420ce5d5288505f431
Marly <marlycormar@gmail.com>
2018-01-16 10:39:45 -0500

    Get @EXTRA-VALID-RANGE values

--- ExternalModule.php
+++ ExternalModule.php
@@ -20,6 +20,8 @@ class ExternalModule extends AbstractExternalModule {
      */
     function redcap_data_entry_form_top($project_id, $record, $instrument, $event_id, $group_id) {
         $this->includeJs('js/complexFieldValidationHelper.js');
+
+
     }
 
     /**
@@ -29,6 +31,41 @@ class ExternalModule extends AbstractExternalModule {
         if (PAGE == 'Design/online_designer.php' && $project_id) {
             $this->includeJs('js/helper.js');
         }
+
+        if($project_id){
+
+            global $Proj;
+
+            foreach (array_keys($Proj->forms[$_GET['page']]['fields']) as $field_name) {
+                $field_info = $Proj->metadata[$field_name];
+                
+                if (!$display_mode = Form::getValueInActionTag($field_info['misc'], $name)) {
+                    continue;
+                }
+
+                // contains the values of the action tag @EXTRA-VALID-RANGES separated
+                // by commas, so if @EXTRA-VALID-RANGES="10,2-34,5", then $field_tag_value
+                // is an array with length 3 and values 10, 2-34, 5.
+                $field_tag_value = explode(",", $display_mode);
+
+
+                //TODO: validate tag values
+
+
+                foreach($field_tag_value as $current){
+                    
+                }
+
+                $tag_values[$field_name] = $display_mode;
+
+
+            }
+
+            //print_r($field_name);
+            $action_tag_values = explode(",", $display_mode);
+
+        }
+
     }
 
 
@@ -53,10 +90,10 @@ class ExternalModule extends AbstractExternalModule {
      */
     protected function getActionTagValue($name) {
         global $Proj;
+
         foreach (array_keys($Proj->forms[$_GET['page']]['fields']) as $field_name) {
-
             $field_info = $Proj->metadata[$field_name];
-
+            
             if ($display_mode = Form::getValueInActionTag($field_info['misc'], $name)) {
                 break;
             }
