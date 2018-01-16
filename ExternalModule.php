fb34224112f929abc1d5260060895e0418cf94e0
Marly <marlycormar@gmail.com>
2018-01-16 11:37:30 -0500

    Create logistic to check ranges and single values

--- ExternalModule.php
+++ ExternalModule.php
@@ -19,33 +19,25 @@ class ExternalModule extends AbstractExternalModule {
      * @inheritdoc
      */
     function redcap_data_entry_form_top($project_id, $record, $instrument, $event_id, $group_id) {
-        $this->includeJs('js/complexFieldValidationHelper.js');
+        $this->includeJs('js/checkRanges.js');
 
         global $Proj;
 
-            foreach (array_keys($Proj->forms[$_GET['page']]['fields']) as $field_name) {
-                $field_info = $Proj->metadata[$field_name];
-                
-                if (!$display_mode = Form::getValueInActionTag($field_info['misc'], $name)) {
-                    continue;
-                }
+        foreach (array_keys($Proj->forms[$_GET['page']]['fields']) as $field_name) {
+            $field_info = $Proj->metadata[$field_name];
+            
+            if (!$display_mode = Form::getValueInActionTag($field_info['misc'], $name)) {
+                continue;
+            }
+            $field_tag_values = explode(",", $display_mode);
 
-                // contains the values of the action tag @EXTRA-VALID-RANGES separated
-                // by commas, so if @EXTRA-VALID-RANGES="10,2-34,5", then $field_tag_values
-                // is an array with length 3 and values 10, 2-34, 5.
-                $field_tag_values = explode(",", $display_mode);
-
-
-                //TODO: validate tag input values
-
-                for($i = 0; $i < sizeof($field_tag_values); $i++){
-                    $field_tag_values[$i] = explode("-", $field_tag_values[$i]);
-
-                }
-
-                $this->sendVarToJS($complex_field_validation_tag_values, $field_tag_values);
+            for($i = 0; $i < sizeof($field_tag_values); $i++){
+                $field_tag_values[$i] = explode("-", $field_tag_values[$i]);
 
             }
+
+            $this->sendVarToJS('complex_field_validation_tag_values', $field_tag_values);
+        }
     }
 
     /**
@@ -53,11 +45,9 @@ class ExternalModule extends AbstractExternalModule {
      */
     function redcap_every_page_top($project_id) {
         if (PAGE == 'Design/online_designer.php' && $project_id) {
-            $this->includeJs('js/helper.js');
+            $this->includeJs('js/addTag.js');
         }
-
     }
-
 
 
     /**
@@ -70,27 +60,6 @@ class ExternalModule extends AbstractExternalModule {
         echo '<script src="' . $this->getUrl($path) . '"></script>';
     }
 
-
-    /**
-     * Returns the assigned value to
-     * a given action tag.
-     *
-     * @param string $path
-     *   The action tag name.
-     */
-    protected function getActionTagValue($name) {
-        global $Proj;
-
-        foreach (array_keys($Proj->forms[$_GET['page']]['fields']) as $field_name) {
-            $field_info = $Proj->metadata[$field_name];
-            
-            if ($display_mode = Form::getValueInActionTag($field_info['misc'], $name)) {
-                break;
-            }
-        }
-
-        return $display_mode;
-    }
 
     /**
      * Sends a PHP variable over to JS.
